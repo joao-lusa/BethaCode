212c3b93fed92eb7d4ffbf748377f07dd1e0786c
João Lucas Pereira Rafael <61843626+joao-lusa@users.noreply.github.com>
2021-09-25 10:18:18 -0300

    crud

--- Java/aula18/LivosApp/src/main/java/aplicacao/LivrosApp.java
+++ Java/aula18/LivosApp/src/main/java/aplicacao/LivrosApp.java
@@ -1,13 +1,65 @@
 package aplicacao;
 
+import modelo.Tipo;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class LivrosApp {
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("minhaConexao");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManagerFactory fabricaGerenciadorEntidades = Persistence.createEntityManagerFactory("minhaConexao");
+        EntityManager geranciadorEntidades = fabricaGerenciadorEntidades.createEntityManager();
+
+            //insersao de dados
+
+//        Tipo tipo1 = new Tipo();
+//        tipo1.setDescricao("Romance");
+//
+//        Tipo tipo2 = new Tipo();
+//        tipo2.setDescricao("Historia");
+//
+//        Tipo tipo3 = new Tipo();
+//        tipo3.setDescricao("Biografia");
+//
+//        geranciadorEntidades.getTransaction().begin();
+//        geranciadorEntidades.persist(tipo1);
+//        geranciadorEntidades.persist(tipo2);
+//        geranciadorEntidades.persist(tipo3);
+//        geranciadorEntidades.getTransaction().commit();
+//
+//        geranciadorEntidades.close();
+//        fabricaGerenciadorEntidades.close();
+
+        //Selecionar no banco de dados
+
+//        Tipo tipoNoBanco = geranciadorEntidades.find(Tipo.class, 6);
+//        System.out.println(tipoNoBanco);
+
+        //update no banco de dados
+
+//        Tipo tipoAtualizar = geranciadorEntidades.find(Tipo.class, 1);
+//        tipoAtualizar.setDescricao("Romance saltess");
+//
+//        geranciadorEntidades.getTransaction().begin();
+//        geranciadorEntidades.persist(tipoAtualizar);
+//        geranciadorEntidades.getTransaction().commit();
+//
+//        geranciadorEntidades.close();
+//        fabricaGerenciadorEntidades.close();
+
+        //remover no banco de dados
+//        Tipo tipoExcluir = geranciadorEntidades.find(Tipo.class, 1);
+//
+//        geranciadorEntidades.getTransaction().begin();
+//        geranciadorEntidades.remove(tipoExcluir);
+//        geranciadorEntidades.getTransaction().commit();
+//
+//        geranciadorEntidades.close();
+//        fabricaGerenciadorEntidades.close();
+
+
+
     }
 
 
