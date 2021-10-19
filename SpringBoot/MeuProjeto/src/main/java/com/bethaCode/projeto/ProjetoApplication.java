b814a1cfe262d519bbcb131d9982f1f049966179
João Lucas Pereira Rafael <61843626+joao-lusa@users.noreply.github.com>
2021-10-19 14:27:16 -0300

    initial commit

---
+++ SpringBoot/MeuProjeto/src/main/java/com/bethaCode/projeto/ProjetoApplication.java
@@ -0,0 +1,11 @@
+package com.bethaCode.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoApplication.class, args);
+    }
+}
