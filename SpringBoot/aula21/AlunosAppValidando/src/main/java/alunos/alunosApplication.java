8554535f2856358ae35ca095d4019669e708deda
João Lucas Pereira Rafael <61843626+joao-lusa@users.noreply.github.com>
2021-10-20 23:28:15 -0300

    Start aula 21

---
+++ SpringBoot/aula21/AlunosAppValidando/src/main/java/alunos/alunosApplication.java
@@ -0,0 +1,10 @@
+package alunos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class alunosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(alunosApplication.class, args);
+    }
+}
