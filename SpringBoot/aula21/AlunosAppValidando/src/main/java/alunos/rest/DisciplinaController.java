20c3b7be759aa740ffd4fe5bff16ef4c938aeaa6
João Lucas Pereira Rafael <61843626+joao-lusa@users.noreply.github.com>
2021-10-21 14:15:46 -0300

    diciplinas criadas

--- SpringBoot/aula21/AlunosAppValidando/src/main/java/alunos/rest/DisciplinaController.java
+++ SpringBoot/aula21/AlunosAppValidando/src/main/java/alunos/rest/DisciplinaController.java
@@ -5,6 +5,7 @@ import alunos.model.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -22,5 +23,37 @@ public class DisciplinaController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Diciplina salvar(@Valid @RequestBody Diciplina disciplina){
         return repository.save(disciplina);
+    }
+
+    @GetMapping("{id}")
+    public Diciplina acharPorId(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina "+id+" não cadastrada!"));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        repository
+                .findById(id)
+                .map(diciplina -> {
+                    repository.delete(diciplina);
+                    return Void.TYPE;
+                })
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina "+id+" não encontrada"));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @Valid @RequestBody Diciplina dadoAtulizado){
+        repository
+                .findById(id)
+                .map(diciplina -> {
+                    diciplina.setDescricao(dadoAtulizado.getDescricao());
+                    diciplina.setNumeroHoras(dadoAtulizado.getNumeroHoras());
+                    return repository.save(diciplina);
+                })
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina "+id+" não cadastrada!"));
     }
 }
